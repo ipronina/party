55dc7e6d7f06c33efe4bd530eaf13963fe1d1301
Irina Pronina <ira-pronina97@bk.ru>
2019-04-21 15:10:10 +0300

    Added modal

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GUESTS } from '../../mocks';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { GuestService } from './services/guest';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,12 +11,39 @@ import { GUESTS } from '../../mocks';
 })
 export class DashboardComponent implements OnInit {
   public guests = GUESTS;
+  public myForm: FormGroup;
 
-  constructor() {}
+  constructor(private modalService: NgbModal, private guestService: GuestService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      surname: new FormControl('', [Validators.required]),
+      gender: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required, Validators.min(18)]),
+      drink: new FormControl('', [Validators.required]),
+    });
+  }
 
   public getValues(object: {}): {} {
     return Object.values(object);
+  }
+
+  public openModal(content) {
+    this.modalService.open(content, { centered: true });
+  }
+
+  public addGuest(form: FormGroup) {
+    if (form.valid) {
+      const guestObj = {
+        name: form.controls.name.value,
+        surname: form.controls.surname.value,
+        gender: form.controls.gender.value,
+        age: form.controls.age.value,
+        drink: form.controls.drink.value,
+      };
+      this.guestService.addGuest(guestObj as any);
+      form.reset();
+    }
   }
 }
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 /* Modules */
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { ModalModule } from '@herbis/ngx-modal';
+import { ReactiveFormsModule } from '@angular/forms';
 /* Components */
 import { DashboardComponent } from './dashboard.component';
+/* Services */
+import { GuestService } from './services/guest';
+import { GuestsService } from './services/guests';
 
 @NgModule({
-  imports: [DashboardRoutingModule, BrowserModule],
+  imports: [DashboardRoutingModule, BrowserModule, ModalModule, ReactiveFormsModule],
   declarations: [DashboardComponent],
+  providers: [GuestService, GuestsService],
 })
 export class DashboardModule {}
