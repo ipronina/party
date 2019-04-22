eef938ee877035597db49df87b486fc26ec770c2
Irina Pronina <ira-pronina97@bk.ru>
2019-04-22 20:43:21 +0300

    WIP: added test for guestService

--- src/app/views/dashboard/services/guest/guest.service.spec.ts
+++ src/app/views/dashboard/services/guest/guest.service.spec.ts
@@ -1,7 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { GuestService } from './guest.service';
 import { GuestsService } from '../guests';
+import { IGuest } from 'src/app/models';
 
 describe('GuestService', () => {
   beforeEach(() =>
@@ -14,4 +15,29 @@ describe('GuestService', () => {
     const service: GuestService = TestBed.get(GuestService);
     expect(service).toBeTruthy();
   });
+
+  it('should called function for updating guests value', inject(
+    [GuestService, GuestsService],
+    (gstService: GuestService, gstsService: GuestsService) => {
+      gstsService.updateGuests = jasmine.createSpy();
+      // const guest = {} as IGuest;
+      // const guests$ = {
+      //   getValue: () => {
+      //     return { 1: {} };
+      //   },
+      // } as any;
+      // expect(gstsService.guestsValue).toEqual({ 1: {}, 2: {} });
+      gstService.assignGuest({} as any);
+      expect(gstsService.updateGuests).toHaveBeenCalled();
+    },
+  ));
+
+  it('should called function for updating guests value', inject(
+    [GuestService, GuestsService],
+    (gstService: GuestService, gstsService: GuestsService) => {
+      gstsService.updateGuests = jasmine.createSpy();
+      gstService.removeGuest(1);
+      expect(gstsService.updateGuests).toHaveBeenCalled();
+    },
+  ));
 });
